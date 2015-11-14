f395aece770428550b85d390d2d9e8e8b7d99031
jasonslyvia <jasonslyvia@gmail.com>
2015-11-14 22:16:27 +0800

    release 1.0.0

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,7 +1,10 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Menu = require('./Menu');
 var menuData = require('./menuData');
 
+window.React = React;
 
-React.render(<Menu menuData={menuData} />, document.querySelector('#demo1 .demo-container'));
-React.render(<Menu menuData={menuData} submenuDirection="below" />, document.querySelector('#demo3 .demo-container'));
+
+ReactDOM.render(<Menu menuData={menuData} />, document.querySelector('#demo1 .demo-container'));
+ReactDOM.render(<Menu menuData={menuData} submenuDirection="below" />, document.querySelector('#demo3 .demo-container'));
